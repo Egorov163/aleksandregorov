20fa4d6dd14e48e7f58839aa3bab052441a0de27
Egorov163 <Aleksandr_egorov_07@mail.ru>
2024-05-07 07:41:30 +0400

    add seed method for stocks

---
+++ all_works/WebStocks/DbStuff/SeedExtension.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WebStocks.DbStuff.Models;
+using WebStocks.DbStuff.Repositories;
+
+namespace WebStocks.DbStuff
+{
+    public static class SeedExtension
+    {
+        public static void Seed(WebApplication app)
+        {
+
+            using (var serviceScope = app.Services.CreateScope())
+            {
+                SeedStock(serviceScope.ServiceProvider);
+            }
+
+        }
+
+        private static void SeedStock(IServiceProvider di)
+        {
+            var stockRepository = di.GetService<StockRepository>();
+            if (!stockRepository.Any())
+            {
+                var stock = new Stock
+                {
+                    Name = "Coca-Cola",
+                    Price = 3000,
+                    IsDeleted = false,
+                    DateBuy = DateTime.Now
+                };
+                stockRepository.Add(stock);
+            }
+        }
+    }
+}
