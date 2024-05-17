6c2b2f4f2246b8685dce722967ac4ec042d9a8fc
Egorov163 <Aleksandr_egorov_07@mail.ru>
2024-05-17 16:04:51 +0400

    update migration

---
+++ all_works/WebStocks/Migrations/20240517114759_deleteRole.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebStocks.Migrations
+{
+    /// <inheritdoc />
+    public partial class deleteRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Stocks_Users_OwnerId",
+                table: "Stocks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OwnerId",
+                table: "Stocks",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Stocks_Users_OwnerId",
+                table: "Stocks",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Stocks_Users_OwnerId",
+                table: "Stocks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OwnerId",
+                table: "Stocks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Stocks_Users_OwnerId",
+                table: "Stocks",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
