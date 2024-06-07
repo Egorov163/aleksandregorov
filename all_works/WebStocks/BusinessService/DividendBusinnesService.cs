edf4dec948a4c85b01516bf0f4cb4fd45506a261
Egorov163 <Aleksandr_egorov_07@mail.ru>
2024-06-07 21:50:55 +0400

    add busness service

---
+++ all_works/WebStocks/BusinessService/DividendBusinnesService.cs
@@ -0,0 +1,35 @@
+﻿using WebStocks.DbStuff.Repositories;
+using WebStocks.Models;
+using WebStocks.Services;
+
+namespace WebStocks.BusinessService
+{
+    public class DividendBusinnesService
+    {
+        private DividendRepository _dividendRepository;
+        private DividendPermissions _dividendPermissions;
+        public DividendBusinnesService(DividendRepository dividendRepository, DividendPermissions dividendPermissions)
+        {
+            _dividendRepository = dividendRepository;
+            _dividendPermissions = dividendPermissions;
+               
+        }
+        public List<DividendViewModel> GetTop10DividendsForMainPage()
+        {
+            var dbDividends = _dividendRepository.Get(10);
+
+            var DividendViewModel = dbDividends.Select(dbDividends => new DividendViewModel
+            {
+                Id = dbDividends.Id,
+                NameStock = dbDividends.Stock.Name,
+                Price = dbDividends.Price,
+                OwnerName = dbDividends.Owner?.Login ?? "Неизвестный",
+                CanDelete = _dividendPermissions.CanDetele(dbDividends)
+
+            })
+                .ToList();
+            return DividendViewModel;
+        }
+
+    }
+}
---
+++ all_works/WebStocks/BusinessService/StockBusinnesService.cs
@@ -0,0 +1,64 @@
+﻿using WebStocks.DbStuff.Models;
+using WebStocks.DbStuff.Repositories;
+using WebStocks.Localizations;
+using WebStocks.Models;
+using WebStocks.Services;
+
+namespace WebStocks.BusinessService
+{
+    public class StockBusinnesService
+    {
+        private StockRepository _stockRepository;
+        private StockPermissions _stockPermissions;
+        public StockBusinnesService(StockRepository stockRepository, StockPermissions stockPermissions)
+        {
+            _stockRepository = stockRepository;
+            _stockPermissions = stockPermissions;
+               
+        }
+        public List<StockViewModel> GetTop10StocksForMainPage()
+        {
+            var dbStocks = _stockRepository.Get(10);
+            var StockVewModel = dbStocks.
+                Select(dbStock => new StockViewModel
+                {
+                    Id = dbStock.Id,
+                    Name = dbStock.Name,
+                    Price = dbStock.Price,
+                    OwnerName = dbStock.Owner?.Login ?? "Неизвестный",
+                    CanDelete = _stockPermissions.CanDetele(dbStock),
+                    CanChange = _stockPermissions.CanChange(dbStock)
+                })
+                .ToList();
+            return StockVewModel;
+        }
+        public List<StockViewModel> GetDeletedStocksForMainPage()
+        {
+            var dbStocks = _stockRepository.GetDeletedStocks();
+            var StockViewModel = dbStocks.
+                Select(dbStock => new StockViewModel
+                {
+                    Id = dbStock.Id,
+                    Name = dbStock.Name,
+                    Price = dbStock.Price,
+                    CanDelete = _stockPermissions.CanDetele(dbStock)
+                })
+                .ToList();
+            return StockViewModel;
+        }
+        public StockInformationViewModel GetStockInformationForMainPage(int stockId)
+        {
+            var dbModel = _stockRepository.GetById(stockId);
+
+            var viewModel = new StockInformationViewModel
+            {
+                Id = dbModel.Id,
+                Name = dbModel.Name,
+                Price = dbModel.Price,
+                LogoUrl = dbModel.LogoUrl,
+                DateBuy = dbModel.DateBuy
+            };
+            return viewModel;
+        }
+    }
+}
