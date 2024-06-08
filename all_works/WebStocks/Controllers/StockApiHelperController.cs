e2c8c3f34b2e8a7cd9edcbdb5a1c8ed3ccf4ccc0
Egorov163 <Aleksandr_egorov_07@mail.ru>
2024-06-08 06:46:19 +0400

    add stockApiHelper

---
+++ all_works/WebStocks/Controllers/StockApiHelperController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStocks.Controllers
+{
+    public class StockApiHelperController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
